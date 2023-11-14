91485bc30563b942f136d38df341f748a3cac5d0
Derek <derek141772@gmail.com>
2023-11-13 21:28:33 -0800

    check gps permission

--- Derek_Huang_MyRuns4/DerekHuangMyRuns1/app/src/main/java/com/example/derek_huang_myruns1/MapActivity.kt
+++ Derek_Huang_MyRuns4/DerekHuangMyRuns1/app/src/main/java/com/example/derek_huang_myruns1/MapActivity.kt
@@ -1,22 +1,32 @@
 package com.example.derek_huang_myruns1
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var saveButton : Button
     private lateinit var cancelButton : Button
     private lateinit var mMap: GoogleMap
+    private val PERMISSION_REQUEST_CODE = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-
+        checkPermission()
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapView)
                 as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -36,6 +46,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT < 23) return
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
     }
 
 }
